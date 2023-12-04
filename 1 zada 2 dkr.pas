062aa1f9888e2599029045763935605126c5cff1
Nikita1719 <144107423+Nikita1719@users.noreply.github.com>
2023-12-04 10:20:55 +0300

    Add files via upload

---
+++ 1 zada 2 dkr.pas
@@ -0,0 +1,16 @@
+﻿var a:array[1..5] of integer;
+    min:integer;
+    max:integer;
+    i:integer;
+begin
+     writeln('введите 5 элементов массива');
+     min:=MAXINT;
+     max:=not(MAXINT);
+     for i:=1 to 5 do begin
+        readln( a[i] );
+        if min>a[i] then min:=a[i];
+        if max<a[i] then max:=a[i];
+     end;
+     writeln( 'Минимальный элемент массива = ', min );
+     writeln( 'Максимальный элемент массива = ', max );
+end.
